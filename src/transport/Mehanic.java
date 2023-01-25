205320a55c08eeb65bc6d2a343ca5cb4aaf47d65
zotin <zotinalexander@gmail.com>
2023-01-25 12:16:28 +0100

    homework done1

--- src/transport/Mehanic.java
+++ src/transport/Mehanic.java
@@ -1,5 +1,7 @@
 package transport;
 
+import java.util.Objects;
+
 public class Mehanic {
     private String name;
     private String company;
@@ -49,5 +51,18 @@ public class Mehanic {
     }
 
     public void maintennance() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mehanic mehanic = (Mehanic) o;
+        return Objects.equals(name, mehanic.name) && Objects.equals(company, mehanic.company) && transportType == mehanic.transportType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, company, transportType);
     }
 }
--- src/transport/Transport.java
+++ src/transport/Transport.java
@@ -4,10 +4,7 @@ import driver.Driver;
 import exception.DiagnosticFaildException;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public  abstract class Transport<T extends Driver> implements Competing {
 
@@ -91,6 +88,19 @@ public  abstract class Transport<T extends Driver> implements Competing {
                 ", model='" + model + '\'' +
                 ", engineVolume=" + engineVolume +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transport<?> transport = (Transport<?>) o;
+        return Double.compare(transport.engineVolume, engineVolume) == 0 && Objects.equals(brand, transport.brand) && Objects.equals(model, transport.model) && Objects.equals(driver, transport.driver) && Objects.equals(mehanics, transport.mehanics);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, driver, mehanics);
     }
 
     public abstract void refill();
